6c09e70da050036b94f6295e0c45021eb9ac400c
archibald1418 <liam141810@gmail.com>
2023-10-23 01:02:29 +0100

    fix regexp

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ MsgFilter: TypeAlias = Callable[[Message], bool]
 
 any_message: MsgFilter = lambda msg: True
 text_message: MsgFilter = lambda msg: msg.content_type == 'text'
+block_handler: MsgFilter = lambda msg: False
 
 
 TOKEN: str  = os.environ["BOT_TOKEN"]
@@ -25,9 +26,26 @@ def send_hello(msg: Message):
 
 
 
-@bot.message_handler(func=TextMessage)
+@bot.message_handler(func=block_handler)
 def echo_all(msg: Message):
+    assert(msg.text)
     bot.reply_to(msg, msg.text)
+
+
+REGEXP_NSECONDS: Pattern[str] = re.compile(r"^\s*?(\d+)\s+?seconds?")
+
+@bot.message_handler(func=text_message)
+def timex(msg: Message):
+    if msg.text:
+        print(repr(msg.text))
+        if (match := re.match(REGEXP_NSECONDS, msg.text)):
+            seconds = int(match.group(1))
+
+            '''Sleep here'''
+            
+            bot.reply_to(msg, f"{seconds} seconds passed!")
+        else:
+            bot.reply_to(msg, "Command is invalid: please write number of seconds")
 
 
 if __name__ == '__main__':
