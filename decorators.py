4f152e642077f0d9ef24824e176980789d26eea3
archibald1418 <liam141810@gmail.com>
2023-10-26 01:14:01 +0100

    add counter decorator

---
+++ decorators.py
@@ -0,0 +1,15 @@
+from typing import Callable, Any, TypeAlias
+
+AnyFunction: TypeAlias = Callable[..., Any]
+
+
+def call_counter(handler: AnyFunction) -> AnyFunction:
+    """Decorator that tracks number of calls to the passed function"""
+
+    def inner(*args, **kwargs):
+        inner.calls += 1  # <- executes in runtime
+        return handler(*args, **kwargs)
+
+    inner.calls = 0  # type: ignore
+
+    return inner
